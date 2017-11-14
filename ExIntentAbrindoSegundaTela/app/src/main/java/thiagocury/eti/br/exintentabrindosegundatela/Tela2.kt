bd846e3c5fdae75952995e735f9b5204f97cff32
Thiago Cury <thiagocury@gmail.com>
2017-11-14 13:21:04 -0200

    Exemplo Intent explicita abrindo a segunda tela

---
+++ ExIntentAbrindoSegundaTela/app/src/main/java/thiagocury/eti/br/exintentabrindosegundatela/Tela2.kt
@@ -0,0 +1,12 @@
+package thiagocury.eti.br.exintentabrindosegundatela
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela2)
+    }
+}
