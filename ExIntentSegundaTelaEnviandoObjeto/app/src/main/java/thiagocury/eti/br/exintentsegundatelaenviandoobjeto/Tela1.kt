30df6571ae5ef19d811aa9f8fe941a08bb5e66bb
thiago <thiagocury@gmail.com>
2019-05-02 15:49:27 -0300

    Ex abrindo segunda tela enviando objeto Intent

---
+++ ExIntentSegundaTelaEnviandoObjeto/app/src/main/java/thiagocury/eti/br/exintentsegundatelaenviandoobjeto/Tela1.kt
@@ -0,0 +1,22 @@
+package thiagocury.eti.br.exintentsegundatelaenviandoobjeto
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_tela1.*
+import org.jetbrains.anko.toast
+
+class Tela1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela1)
+
+        //Recuperando Objeto fazendo CAST com a palavra "as" para a classe Pessoa
+        var p = intent.getSerializableExtra("pessoa") as Pessoa
+
+        //Recebendo o valor digitado
+        t1_tv_msg.text = "Olá, ${p.toString()}"
+
+        toast(resources.getText(R.string.t1_toast_msg))
+    }
+}
