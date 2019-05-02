ed5bb7c348d693827da8a5ead34530a006b61711
thiago <thiagocury@gmail.com>
2019-05-02 10:58:12 -0300

    Ex Toast

---
+++ ExToast/app/src/main/java/thiagocury/eti/br/extoast/MainActivity.kt
@@ -0,0 +1,34 @@
+package thiagocury.eti.br.extoast
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        Site lib Anko: https://github.com/Kotlin/anko
+//        Para utilizar a Anko precisamos:
+//        Adicionar em buildscript nível do projeto
+//        ext.anko_version = '0.10.8' //Não esquecer de verificar por versões mais atuais.
+//        Utilizar o implementation a seguir:
+//        implementation "org.jetbrains.anko:anko:$anko_version"
+
+        //Dessa forma não estamos internacionalizando o projeto.
+        toast("Olá, mensagem enviada via Toast!");
+
+        /* Dessa forma estamos internacionalizando o projeto.
+        *  Note que estamos chamando resources().getString(), assim
+        *  mantemos a internacionalização do nosso projeto com base nas variáveis criadas
+        *  dentro do strings.xml */
+        toast(resources.getString(R.string.toast_message));
+
+        /* Chamando toast normal ela vai permanecer na tela conforme o tempo de: Toast.LENGTH_SHORT
+         * enquanto o método longToast vai permanecer conforme: Toast.LENGTH_LONG */
+        longToast(resources.getString(R.string.long_toast_message));
+    }
+}
